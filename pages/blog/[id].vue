93477f03911d5be55685168cbf6b21cdcb50f030
Holson Mpangala <holsonm74@gmail.com>
2024-10-25 22:49:25 +0100

    Update : Data UseFetch

--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -1,8 +1,14 @@
 <template>
     <main>
-        <h1>{{ route.params.id }}</h1>
+        <h1>{{ post.title }}</h1>
+        <div>
+            
+        </div>
     </main>
 </template>
 <script setup>
     const route = useRoute();
+    const { data:post, pending, error, refresh } = await useFetch('https://jsonplaceholder.typicode.com/posts/' + route.params.id,{
+        
+    })
 </script>
