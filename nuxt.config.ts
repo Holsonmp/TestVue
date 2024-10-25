455f00508a8418b7e240cc96cf4371d32a83693e
Holson Mpangala <holsonm74@gmail.com>
2024-10-25 23:13:43 +0100

    Add : pageTransition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,11 @@ export default defineNuxtConfig({
 		head:{
 			charset: 'utf-8',
 			viewport: 'width=device-width, initial-scale=1'
+		},
+		pageTransition: { 
+			name: 'page', 
+			mode: 'out-in' 
 		}
 	}
 })
+
