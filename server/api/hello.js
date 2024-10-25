2ab92204a19dfe22c576d16ccde23f03c1013b77
Holson Mpangala <holsonm74@gmail.com>
2024-10-25 23:53:35 +0100

    Fix : Json format

--- server/api/hello.js
+++ server/api/hello.js
@@ -1,3 +1,5 @@
 export default defineEventHandler((event)=>{
-    return 'Hello Mot';
+    return {
+        hello : 'Hello. Our agency has been present for over 29 years in the market. We make the most of all our customers.'
+    }
 })
